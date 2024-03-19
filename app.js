82cd57d30486f9b96b62500f371da9acfd62de70
ANDRÉS R. BUCHELI (Andrés F. Regalado Bucheli) <radio_seattle@hotmail.com>
2024-03-19 11:25:52 +0300

    Update app.js

--- app.js
+++ app.js
@@ -13,7 +13,7 @@ const html = `
 <!DOCTYPE html>
 <html>
   <head>
-    <title>Hello from Render!</title>
+    <title><a href="https://github.com/ARBUCHELI/DEPLOYING-WITH-RENDER">HOW TO DEPLOY AND APPLICATION WITH RENDER!</a></title>
     <script src="https://cdn.jsdelivr.net/npm/canvas-confetti@1.5.1/dist/confetti.browser.min.js"></script>
     <script>
       setTimeout(() => {
